e96342f4ab6845947fddc83a79fc057b1c6d3513
Oliver <olivergent@btinternet.com>
2022-09-16 16:58:42 +0100

    add mass input and start LCD stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include "HX711.h"
+#include "LiquidCrystal_I2C.h"
 
 /**
  * An example useage of the HX711 librabry https://github.com/bogde/HX711/blob/master/examples/HX711_full_example/HX711_full_example.ino
+ * Screen example https://github.com/johnrickman/LiquidCrystal_I2C/blob/master/examples/HelloWorld/HelloWorld.pde
  */
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+HX711 C1_scale;
+HX711 C2_scale;
+HX711 C3_scale;
+HX711 C4_scale;
 
 // HX711 circuit wiring, the 8 HX711 data pins can use an digitial pin
 const int C1_LOADCELL_DOUT_PIN = 3;
@@ -13,11 +21,6 @@ const int C3_LOADCELL_DOUT_PIN = 7;
 const int C3_LOADCELL_SCK_PIN = 6;
 const int C4_LOADCELL_DOUT_PIN = 9;
 const int C4_LOADCELL_SCK_PIN = 8;
-
-HX711 C1_scale;
-HX711 C2_scale;
-HX711 C3_scale;
-HX711 C4_scale;
 
 float C1_reading;
 float C2_reading;
@@ -47,7 +50,8 @@ void setup()
     C4_scale.get_scale();
     C4_scale.tare();
 
-    Serial.println("place a weight at the centre of the plate, or anything if you just want position");
+    Serial.println("place a known mass at the centre of the plate if you want a correct total mass reading");
+    Serial.println("place any mass at the centre of the plate otherwise");
     Serial.println("you have 5 seconds");
     delay(5000);
 
@@ -56,11 +60,18 @@ void setup()
     long r3 = C3_scale.get_units(10);
     long r4 = C4_scale.get_units(10);
 
-    C1_scale.set_scale(r1 / 1000);
-    C2_scale.set_scale(r2 / 1000);
-    C3_scale.set_scale(r3 / 1000);
-    C4_scale.set_scale(r4 / 1000);
+    Serial.println("Enter mass (g) or press enter for unknown mass :");
+    String mass_input = Serial.readStringUntil('\n');
+    int mass = mass_input.toInt();
+    if(mass==0) // mass will be 0 if they just pressed enter
+        mass = 1000;
+
+    C1_scale.set_scale(r1 / mass);
+    C2_scale.set_scale(r2 / mass);
+    C3_scale.set_scale(r3 / mass);
+    C4_scale.set_scale(r4 / mass);
 }
+
 
 void loop()
 {
