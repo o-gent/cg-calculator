c08ae16ea124eb4b20e4509cef639cd0da329e4c
Oliver <olivergent@btinternet.com>
2022-09-20 12:57:41 +0100

    impliment screen + refactor + switch

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 From 4 strain gauges at the corners of a plate, calculate where a mass is on the plate
 
 Currently uses an arduino uno and 4 HX711s
+
+to buy:
+- upgraded load cell amplifier https://thepihut.com/products/sparkfun-qwiic-scale-nau7802
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "HX711.h"
 #include "LiquidCrystal_I2C.h"
+#include <EEPROM.h>
 
 /**
  * An example useage of the HX711 librabry https://github.com/bogde/HX711/blob/master/examples/HX711_full_example/HX711_full_example.ino
@@ -22,54 +23,139 @@ const int C3_LOADCELL_SCK_PIN = 6;
 const int C4_LOADCELL_DOUT_PIN = 9;
 const int C4_LOADCELL_SCK_PIN = 8;
 
-float C1_reading;
-float C2_reading;
-float C3_reading;
-float C4_reading;
+float r1;float r2;float r3;float r4;
+int cal_mass = 1000;
+float C1_reading; float C2_reading;
+float C3_reading; float C4_reading;
 float total;
+bool CALIBRATE;
 
-
-void setup()
+/**
+ * routine to print basic info + tare scales
+ */
+void startup()
 {
-    Serial.begin(57600);
+
+    pinMode(12, INPUT_PULLUP);
+    CALIBRATE = digitalRead(12);
+
+    lcd.init();
+    lcd.backlight();
+    lcd.clear();
+
+    lcd.setCursor(0,0);
+    lcd.print("--------------------");
+    lcd.setCursor(0,3);
+    lcd.print("--------------------");
+    lcd.setCursor(0,1);
+    lcd.print("CL CG finder");
+    delay(1000);
+    lcd.clear();
+    
+    lcd.setCursor(0,0);
+    lcd.print("Calibration:");
+    CALIBRATE == 1 ? lcd.print("true") : lcd.print("false");
+    delay(2000);
+    lcd.clear();
+    
     C1_scale.begin(C1_LOADCELL_DOUT_PIN, C1_LOADCELL_SCK_PIN);
     C2_scale.begin(C2_LOADCELL_DOUT_PIN, C2_LOADCELL_SCK_PIN);
     C3_scale.begin(C3_LOADCELL_DOUT_PIN, C3_LOADCELL_SCK_PIN);
     C4_scale.begin(C4_LOADCELL_DOUT_PIN, C4_LOADCELL_SCK_PIN);
 
-    Serial.println("Calibrate start, clear scales");
-    C1_scale.get_scale();
-    C1_scale.tare();
+    Serial.println("tare start, clear scales");
+    
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Tare: Clear Scales");
+    lcd.setCursor(0,1); lcd.print("3"); delay(1500);
+    lcd.setCursor(0,2); lcd.print("2"); delay(1500);
+    lcd.setCursor(0,3); lcd.print("1"); delay(1500);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("keep clear..");
 
-    C2_scale.get_scale();
-    C2_scale.tare();
+    
+    C1_scale.get_scale(); C1_scale.tare();
+    C2_scale.get_scale(); C2_scale.tare();
+    C3_scale.get_scale(); C3_scale.tare();
+    C4_scale.get_scale(); C4_scale.tare();
+}
 
-    C3_scale.get_scale();
-    C3_scale.tare();
 
-    C4_scale.get_scale();
-    C4_scale.tare();
-
-    Serial.println("place a known mass at the centre of the plate if you want a correct total mass reading");
+/**
+ * Routine to get the calibration values for the scales
+ */
+void calibrate()
+{
+    Serial.println("place a known cal_mass at the centre of the plate if you want a correct total mass reading");
     Serial.println("place any mass at the centre of the plate otherwise");
     Serial.println("you have 5 seconds");
-    delay(5000);
 
-    long r1 = C1_scale.get_units(10);
-    long r2 = C2_scale.get_units(10);
-    long r3 = C3_scale.get_units(10);
-    long r4 = C4_scale.get_units(10);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Place known weight");
+    lcd.setCursor(0,1); lcd.print("3"); delay(1500);
+    lcd.setCursor(0,2); lcd.print("2"); delay(1500);
+    lcd.setCursor(0,3); lcd.print("1"); delay(1500);
+    lcd.clear();
 
-    Serial.println("Enter mass (g) or press enter for unknown mass :");
-    String mass_input = Serial.readStringUntil('\n');
-    int mass = mass_input.toInt();
-    if(mass==0) // mass will be 0 if they just pressed enter
-        mass = 1000;
+    r1 = C1_scale.get_units(10);
+    r2 = C2_scale.get_units(10);
+    r3 = C3_scale.get_units(10);
+    r4 = C4_scale.get_units(10);
 
-    C1_scale.set_scale(r1 / mass);
-    C2_scale.set_scale(r2 / mass);
-    C3_scale.set_scale(r3 / mass);
-    C4_scale.set_scale(r4 / mass);
+    Serial.println("Enter mass (g) or press enter for unknown mass (you have 20s) :");
+    Serial.setTimeout(20000);
+    lcd.setCursor(0,0);
+    lcd.print("enter mass through");
+    lcd.setCursor(0,1);
+    lcd.print("serial connection.");
+    lcd.setCursor(0,2);
+    lcd.print("press enter after");
+    lcd.setCursor(0,2);
+    lcd.print("number : )");
+    String cal_mass_input = Serial.readStringUntil('\n');
+    cal_mass = cal_mass_input.toInt();
+    if(cal_mass==0) // cal_mass will be 0 if they just pressed enter
+        cal_mass = 1000;
+    // Store the calibrated value
+    EEPROM.put(0, cal_mass);
+    EEPROM.put(10, r1);
+    EEPROM.put(20, r2);
+    EEPROM.put(30, r3);
+    EEPROM.put(40, r4);
+}
+
+void setup()
+{
+    Serial.begin(57600);
+
+    startup();
+
+    if(CALIBRATE)
+        calibrate();
+    else {
+        Serial.println("Using stored calibration value");
+        Serial.println("Set CALIBRATE to true in code to recal");
+        delay(4000);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Using stored cal val");
+        lcd.setCursor(0,2);
+        lcd.print("use switch to re-cal");
+    }
+
+    // read in the calibrated mass value from EEPROM
+    EEPROM.get(0, cal_mass);
+    EEPROM.get(10, r1); EEPROM.get(20, r2);
+    EEPROM.get(30, r3); EEPROM.get(40, r4);
+    Serial.println(cal_mass);
+
+    C1_scale.set_scale(r1 / cal_mass);
+    C2_scale.set_scale(r2 / cal_mass);
+    C3_scale.set_scale(r3 / cal_mass);
+    C4_scale.set_scale(r4 / cal_mass);
 }
 
 
@@ -79,10 +165,10 @@ void loop()
      * Sensor readings and math
      */
     
-    C1_reading = C1_scale.get_units(1);
-    C2_reading = C2_scale.get_units(1);
-    C3_reading = C3_scale.get_units(1);
-    C4_reading = C4_scale.get_units(1);
+    C1_reading = C1_scale.get_units(5);
+    C2_reading = C2_scale.get_units(5);
+    C3_reading = C3_scale.get_units(5);
+    C4_reading = C4_scale.get_units(5);
     total = C1_reading + C2_reading + C3_reading + C4_reading;
 
     // converting CX_reading to float to do float division.
@@ -91,6 +177,9 @@ void loop()
     float C2_load = ((float)C2_reading) / total;
     float C3_load = ((float)C3_reading) / total;
     float C4_load = ((float)C4_reading) / total;
+
+    float X = (C4_load + C3_load) / 1 - (C1_load + C2_load) / 1;
+    float Y = (C1_load + C4_load) / 1 - (C2_load + C3_load) / 1;
 
     /**
      * Serial debugging output
@@ -105,20 +194,25 @@ void loop()
     Serial.println(C4_load);
     
     Serial.print("X: ");
-    Serial.println((C4_load + C3_load) / 1 - (C1_load + C2_load) / 1);
+    Serial.println(X);
     Serial.print("Y: ");
-    Serial.println((C1_load + C4_load) / 1 - (C2_load + C3_load) / 1);
+    Serial.println(Y);
     
     Serial.println();
 
     /**
      * LCD display
      */
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("T W:");
+    lcd.print(total);
 
-    lcd.backlight();
-    lcd.setCursor(3,0);
-    lcd.print("Hello, world!");
+    lcd.setCursor(0,1);
+    lcd.print("X:");
+    lcd.print(X);
     
-    
-    delay(500);
+    lcd.setCursor(0,2);
+    lcd.print("Y:");
+    lcd.print(Y);
 }
